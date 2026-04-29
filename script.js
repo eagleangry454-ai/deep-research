7fe9eebe34fedc4a58a3259b4d5c49019f1cba59
eagleangry454-ai <eagleangry454@gmail.com>
2026-04-29 18:17:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function search() {
   let query = document.getElementById("searchBox").value;
 
   let response = await fetch(
-    `https://serpapi.com/search.json?q=${query}&api_key=YOUR_API_KEY`
+    `https://serpapi.com/search.json?q=${query}&api_key=4ba20149b93da80f59bdd6ba6394fca12169ec0d194f911a0090ab7f8bec3866
   );
 
   let data = await response.json();
