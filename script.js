49ab1f799a5025364395cc325a544fc8e5c20194
eagleangry454-ai <eagleangry454@gmail.com>
2026-04-30 17:25:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-async function search() {
+function search() {
   let query = document.getElementById("searchBox").value;
-
-  let response = await fetch(
-    `https://serpapi.com/search.json?q=${query}&api_key=4ba20149b93da80f59bdd6ba6394fca12169ec0d194f911a0090ab7f8bec3866
-  );
-
-  let data = await response.json();
-
-  let resultsDiv = document.getElementById("results");
-  resultsDiv.innerHTML = "";
-
-  data.organic_results.forEach(item => {
-    resultsDiv.innerHTML += `
-      <p>
-        <a href="${item.link}" target="_blank">
-          ${item.title}
-        </a>
-      </p>
-    `;
-  });
+  alert("Searching for: " + query);
 }
